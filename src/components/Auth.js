7390914cfc12e95c41f3acf043a8152125dcbf50
Alexander Bocharov <fireon4ik229@mail.ru>
2024-07-01 12:22:24 +0700

    mb it`s work

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,35 +3,47 @@ import React from 'react';
 
 class CRMIntegration extends React.Component {
   componentDidMount() {
+    if (!window.BX24) {
+      console.error('BX24 is not available');
+      return;
+    }
+
     this.initBX24();
+
     if (!localStorage.getItem('placementBound')) {
       this.bindPlacement();
     }
   }
 
   initBX24() {
-    if (window.BX24) {
-      BX24.init(() => {
-        BX24.getAuth((authData) => {
-          console.log(authData);
-        });
+    BX24.init(() => {
+      BX24.getAuth((authData) => {
+        if (authData.error) {
+          console.error('Error getting auth data:', authData.error);
+          return;
+        }
+
+        console.log(authData);
       });
-    }
+    });
   }
 
   bindPlacement() {
-    if (window.BX24) {
-      BX24.callMethod('placement.bind', {
-        PLACEMENT:'CRM_DEAL_DETAIL_TAB',
-        HANDLER: window.location.href,
-        TITLE: 'Custom tab'
-      }, (result) => {
-        console.log(result);
-        if (result.answer.result) {
-          localStorage.setItem('placementBound', 'true');
-        }
-      });
-    }
+    BX24.callMethod('placement.bind', {
+      PLACEMENT:'CRM_DEAL_DETAIL_TAB',
+      HANDLER: window.location.href,
+      TITLE: 'Расходы по сделкам'
+    }, (result) => {
+      if (result.error()) {
+        console.error('Error binding placement:', result.error());
+        return;
+      }
+
+      console.log(result);
+      if (result.answer.result) {
+        localStorage.setItem('placementBound', 'true');
+      }
+    });
   }
 
   render() {
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,6 +1,6 @@
+/* global BX24 */
 import React, { useState, useEffect } from 'react';
 import { Button, Modal } from 'react-bootstrap';
-import axios from 'axios';
 import ExpenseForm from './ExpenseForm';
 import ExpenseTable from './ExpenseTable';
 
@@ -14,33 +14,42 @@ const Expenses = () => {
   const [forceUpdate, setForceUpdate] = useState(false);
 
   // Конфигурация для запросов к API
-  const webhookUrl = 'https://b24-9zmx9g.bitrix24.ru/rest/1/iu0e2vslb0030jdz';
   const listId = '31';
 
   // Загрузка данных о расходах при монтировании компонента
   useEffect(() => {
-    const fetchExpenses = async () => {
-      try {
-        const response = await axios.get(`${webhookUrl}/lists.element.get`, {
-          params: {
-            IBLOCK_TYPE_ID: 'lists',
-            IBLOCK_ID: listId
+    BX24.init(() => {
+      console.log("BX24 is ready", BX24.isAdmin());
+
+      const auth = BX24.getAuth();
+      if (auth) {
+        const fetchExpenses = async () => {
+          try {
+            BX24.callMethod('lists.element.get', {
+              IBLOCK_TYPE_ID: 'lists',
+              IBLOCK_ID: listId,
+              auth: auth.access_token
+            }, function(result) {
+              if(result.error()) {
+                console.error('Ошибка при загрузке расходов:', result.error());
+              } else {
+                console.log('Полученные данные:', result.data());
+                if (result.data()) {
+                  setExpenses(result.data());
+                }
+              }
+            });
+          } catch (error) {
+            console.error('Ошибка при загрузке расходов:', error);
           }
-        });
+        };
 
-        console.log('Полученные данные:', response.data);
-        if (response.data.result) {
-          setExpenses(response.data.result);
-        } else {
-          console.error('Ошибка при загрузке расходов:', response.data);
-        }
-      } catch (error) {
-        console.error('Ошибка при загрузке расходов:', error);
+        fetchExpenses();
+      } else {
+        console.error('Ошибка при получении токена доступа');
       }
-    };
-
-    fetchExpenses();
-  }, [webhookUrl, listId, forceUpdate]);
+    });
+  }, [listId, forceUpdate]);
 
   // Функции для управления состоянием модального окна
   const handleClose = () => setShow(false);
@@ -49,41 +58,62 @@ const Expenses = () => {
   // Функция для добавления нового расхода
   const addExpense = async (expense) => {
     try {
-      const currentUser = '123456'; // Заглушка для идентификатора пользователя
+      BX24.callMethod('user.current', {}, function(result) {
+        if(result.error()) {
+          console.error('Ошибка при получении текущего пользователя:', result.error());
+        } else {
+          const currentUserId = result.data().ID;
 
-      const newExpense = {
-        IBLOCK_TYPE_ID: 'lists',
-        IBLOCK_ID: listId,
-        ELEMENT_CODE: `expense_${Date.now()}`, // Уникальный код элемента
-        FIELDS: {
-          NAME: expense.purpose,
-          PROPERTY_125: expense.type,
-          PROPERTY_113: formatDate(expense.plannedDate),
-          PROPERTY_117: expense.amount,
-          PROPERTY_127: currentUser,
-          PROPERTY_129: expense.dealLink
+          BX24.callMethod('user.get', { 'ID': currentUserId }, function(userResult) {
+            if(userResult.error()) {
+              console.error('Ошибка при получении данных пользователя:', userResult.error());
+            } else {
+              console.log('Полученные данные пользователя:', userResult.data());
+
+              // Выбираем первый элемент массива
+              const userData = userResult.data()[0];
+
+              const currentUser = `${userData.NAME} ${userData.LAST_NAME}`;
+
+              const newExpense = {
+                IBLOCK_TYPE_ID: 'lists',
+                IBLOCK_ID: listId,
+                ELEMENT_CODE: `expense_${Date.now()}`, // Уникальный код элемента
+                FIELDS: {
+                  NAME: expense.purpose,
+                  PROPERTY_125: expense.type,
+                  PROPERTY_113: formatDate(expense.plannedDate),
+                  PROPERTY_117: expense.amount,
+                  PROPERTY_127: currentUser,
+                  PROPERTY_129: expense.dealLink
+                }
+              };
+
+              if (expense.actualDate) {
+                newExpense.FIELDS.PROPERTY_115 = formatDate(expense.actualDate);
+              }
+
+              console.log('Отправляемый новый расход:', newExpense);
+
+              BX24.callMethod('lists.element.add', newExpense, function(result) {
+                if(result.error()) {
+                  console.error('Ошибка при добавлении расхода:', result.error());
+                } else {
+                  console.log('Ответ после добавления расхода:', result.data());
+                  if (result.data()) {
+                    const updatedExpenses = [...expenses, { ...newExpense.FIELDS, ID: result.data() }];
+                    setExpenses(updatedExpenses);
+                    setForceUpdate(prev => !prev); // Обновление состояния для перерисовки компонента
+                    handleClose();
+                  }
+                }
+              });
+            }
+          });
         }
-      };
-
-      if (expense.actualDate) {
-        newExpense.FIELDS.PROPERTY_115 = formatDate(expense.actualDate);
-      }
-
-      console.log('Отправляемый новый расход:', newExpense);
-
-      const response = await axios.post(`${webhookUrl}/lists.element.add`, newExpense);
-      console.log('Ответ после добавления расхода:', response.data);
-
-      if (response.data.result) {
-        const updatedExpenses = [...expenses, { ...newExpense.FIELDS, ID: response.data.result }];
-        setExpenses(updatedExpenses);
-        setForceUpdate(prev => !prev); // Обновление состояния для перерисовки компонента
-        handleClose();
-      } else {
-        console.error('Ошибка при добавлении расхода:', response.data);
-      }
+      });
     } catch (error) {
-      console.error('Ошибка при добавлении расхода:', error.response ? error.response.data : error);
+      console.error('Ошибка при добавлении расхода:', error);
     }
   };
 
